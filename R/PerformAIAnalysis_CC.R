b54912933d1b3b4746c978ebb0b5228062ce3be4
Asya Mendelevich <mendelevich@179.ru>
2019-03-29 15:49:13 -0400

    finished, ready for the first set of tests

--- R/PerformAIAnalysis_CC.R
+++ R/PerformAIAnalysis_CC.R
@@ -96,7 +96,8 @@ MixBetaBinomialFit <- function(initials, coverage, observations){
   initials_old <- initials
   initials_new <- MixBetaBinomialFitStep(initials_old, coverage, observations)
   n_steps <- 1
-  while(all(abs(initials_new - initials_old) > 0.001)){
+  while(all(abs(initials_new - initials_old) > 0.001) &
+        initials_new[3] < 2){
     initials_old <- initials_new
     initials_new <- MixBetaBinomialFitStep(initials_old, coverage, observations)
     n_steps = n_steps + 1
@@ -255,3 +256,75 @@ ComputeCorrConstantFor2Reps <- function(inDF, reps, binNObs=40,
   ))
 
 }
+
+
+PerformBinTestAIAnalysisForConditionNPoint <- function(inDF, vectReps, pt = 0.5,
+                                                       Q=0.95, EPS=1.3, thr=NA, thrUP=NA, thrType="each",
+                                                       minDifference=NA){
+  #' Input: data frame with gene names and counts (reference and alternative) + numbers of replicates to use for condition + point estimate to compare
+  #'
+  #' @param inDF A table with ref & alt counts per gene/SNP for each replicate plus the first column with gene/SNP names
+  #' @param vectReps A vector (>=2) of replicate numbers that should be considered as tech reps
+  #' @param condName An optional parameter; one-word name for condition
+  #' @param Q An optional parameter; %-quantile (for example 0.95, 0.8, etc)
+  #' @param thr An optional parameter; threshold on the overall number of counts (in all replicates combined) for a gene to be considered
+  #' @param thrUP An optional parameter for a threshold for max gene coverage (default = NA)
+  #' @param thrType An optional parameter for threshold type (default = "each", also can be "average" coverage on replicates)
+  #' @param fullOUT Set true if you want full output with all computationally-internal dfs
+  #' @return A table of gene names, AIs + CIs, classification into genes demonstrating differential from point estimate AI and those that don't
+  #' @examples
+  #'
+
+  repCombs <- combn(vectReps, 2)
+
+  fitDATA <- lapply(1:ncol(repCombs), function(j){
+    x = repCombs[, j]
+    ComputeCorrConstantFor2Reps(inDF=SM100pg_20mln, reps=x,
+                                EPS=EPS, thr=thr, thrUP=thrUP, thrType=thrType)
+  })
+
+  vectRepsCombsCC <- sapply(1:ncol(repCombs), function(j){
+    fitDATA[[j]]$fittedCC
+  })
+  CC <- mean(vectRepsCombsCC)
+
+  ### FOR PAIR!!!
+
+  AI <- CountsToAI(inDF, reps=vectReps, meth="mergedToProportion", thr=thr, thrUP=thrUP, thrType=thrType)$AI
+  tmpDF  <- ThresholdingCounts(inDF, reps=vectReps, thr=thr, thrUP=thrUP, thrType=thrType)
+  sumCOV <- rowSums(tmpDF[, -1])
+  matCOV <- rowSums(tmpDF[, seq(2,ncol(tmpDF),2)])
+
+  DF <- data.frame(ID=inDF[,1], sumCOV=sumCOV, matCOV=matCOV, AI=AI)
+
+  # Bonferroni correction:
+  Qbf <- 1 - (1-Q)/nrow(na.omit(DF))
+
+  # Bin test:
+  tmpDFbt <- t(sapply(1:nrow(DF), function(i){
+    if(is.na(matCOV[i]) | is.na(sumCOV[i])) { return(c(NA,NA,NA)) }
+    BT <- binom.test(matCOV[i], sumCOV[i], alternative="two.sided", conf.level = Qbf, p=pt)
+    c(BT$p.value, BT$conf.int[1], BT$conf.int[2])
+  }))
+
+  DF$BT_pval = tmpDFbt[, 1]
+  DF$BT_CIleft = tmpDFbt[, 2]
+  DF$BT_CIright = tmpDFbt[, 3]
+
+  DF$BT_CIleft_CC <- sapply(DF$AI - (DF$AI - DF$BT_CIleft) * CC,
+                           function(lb){ max(0, lb) })
+  DF$BT_CIright_CC <- sapply(DF$AI + (DF$BT_CIright - DF$AI) * CC,
+                           function(ub){ min(1, ub) })
+
+  DF$BT <- (DF$BT_pval < 0.05)
+
+  # Find intersecting intervals > call them FALSE (non-rejected H_0)
+  DF$BT_CC <- !(DF$BT_CIleft_CC <= pt & DF$BT_CIright_CC >= pt)
+  if (!is.na(minDifference))
+  {
+    DF$BT_CC_thrDiff <- (DF$BT_CC & (abs(DF$AI - pt) >= minDifference))
+  }
+
+  return(list(corrConst = CC, output = DF))
+}
+
