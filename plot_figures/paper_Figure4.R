beb1651bb497a66b70e4191b585dd6ca73be42d2
kintany <kintany@gmail.com>
2019-07-26 18:53:05 -0400

    figure 4 code

--- plot_figures/paper_Figure4.R
+++ plot_figures/paper_Figure4.R
@@ -26,7 +26,7 @@ res62_df <-
     for_6_df <- CountsToAI(data_30mln_noX[[m]], reps_6_from30[[m]], meth="mergedToProportion")$AI
     df <- sapply(1:length(list_of_consts[[m]]), function(j){
       pairs_sample = reps_6_from30[[m]][combn(list_of_Nreps[[m]], 2)[, j]]
-      res62 <- PerformBinTestAIAnalysisForConditionNPointVect_knownCC(data_30mln_noX[[m]],   
+      res62 <- PerformBinTestAIAnalysisForConditionNPointVect_knownCC(data_30mln_noX[[m]],
                                                                       vectReps=pairs_sample,
                                                                       vectRepsCombsCC = list_of_consts[[m]][j],
                                                                       ptVect = for_6_df,
@@ -61,7 +61,7 @@ res61_df <-
   lapply(1:3, function(m){
     for_6_df <- CountsToAI(data_30mln_noX[[m]], reps_6_from30[[m]], meth="mergedToProportion")$AI
     df <- sapply(1:list_of_Nreps[[m]], function(j){
-      res61 <- PerformBinTestAIAnalysisForConditionNPointVect_knownCC(data_30mln_noX[[m]],  #list_of_datas[[m]], 
+      res61 <- PerformBinTestAIAnalysisForConditionNPointVect_knownCC(data_30mln_noX[[m]],  #list_of_datas[[m]],
                                                                       vectReps=reps_6_from30[[m]][j],
                                                                       vectRepsCombsCC = list_of_consts[[m]][j],
                                                                       ptVect = for_6_df,
@@ -152,6 +152,19 @@ sdPairs = lapply(1:3, function(l){
   })
 })
 
+slopesG1 = sapply(unique(dataCexlALLlogG1$what), function(what){
+  sapply(unique(dataCexlALLlogG1$who), function(who){
+    sqrt(10**(lm(dataCexlALLlogG1[dataCexlALLlogG1$who==who & dataCexlALLlogG1$what==what &
+                                    dataCexlALLlogG1$vc!= -Inf, ], formula = (vc ~ offset(mc)))$coefficients))
+  })
+})
+slopesG10 = sapply(unique(dataCexlALLlogG10$what), function(what){
+  sapply(unique(dataCexlALLlogG10$who), function(who){
+    sqrt(10**lm(dataCexlALLlogG10[dataCexlALLlogG10$who==who & dataCexlALLlogG10$what==what, ], formula = (vc ~ offset(mc)))$coefficients)
+  })
+})
+
+
 CC_ov_sdp_df = data.frame(QCC = unlist(CC)[6:45],
                           sd_pair = unlist(sdPairs),
                           overdispersion_sqr = rep(slopesG10[,1], each=15)[6:45],
@@ -171,9 +184,9 @@ df_1exp = lapply(1:3, function(i){
                         function(j){
                           pair1 = combn(reps_6_from30[[i]],2)[,j]
                           rest1 = reps_6_from30[[i]][! reps_6_from30[[i]] %in% pair1]
-                          df = data.frame(X1=rep(pair1[1], each=ncol(combn(rest1, 2))), 
+                          df = data.frame(X1=rep(pair1[1], each=ncol(combn(rest1, 2))),
                                           X2=rep(pair1[2], each=ncol(combn(rest1, 2))),
-                                          X3=(combn(rest1, 2))[1,], 
+                                          X3=(combn(rest1, 2))[1,],
                                           X4=(combn(rest1, 2))[2,],
                                           CC_12 = CC[[i]][j])
                           df$CC_34 = sapply(1:nrow(df), function(k){
@@ -187,17 +200,17 @@ set.seed(3); df_1exp_15 = lapply(df_1exp, function(x){x[sample(nrow(x),15), ]})
 set.seed(4); df_2exp_15 = lapply(1:ncol(combn(1:3,2)), function(c){
   m1 = combn(1:3,2)[1,c]
   m2 = combn(1:3,2)[2,c]
-  data.frame(df_2exp[[m1]][sample(1:nrow(df_2exp[[m1]]),15), ], 
+  data.frame(df_2exp[[m1]][sample(1:nrow(df_2exp[[m1]]),15), ],
              df_2exp[[m2]][sample(1:nrow(df_2exp[[m2]]),15), ])
 })
 
 interrep_exp_diff = lapply(1:3, function(df_1exp_n){
   lapply(1:15, function(i){
     data1 = df_1exp_15[[df_1exp_n]][i, ]
-    df = data_30mln_noX[[df_1exp_n]][, c(1, 
+    df = data_30mln_noX[[df_1exp_n]][, c(1,
                                          sort(c(data1$X1*2, data1$X1*2+1, data1$X2*2, data1$X2*2+1)),
                                          sort(c(data1$X3*2, data1$X3*2+1, data1$X4*2, data1$X4*2+1)))]
-    PerformBinTestAIAnalysisForTwoConditions_knownCC(df, vect1CondReps=1:2, vect2CondReps=3:4, 
+    PerformBinTestAIAnalysisForTwoConditions_knownCC(df, vect1CondReps=1:2, vect2CondReps=3:4,
                                                      vect1CondRepsCombsCC=data1$CC_12, vect2CondRepsCombsCC=data1$CC_34,
                                                      Q=0.95, thr=10, thrUP=NA, thrType="each", minDifference=NA)
   })
@@ -212,7 +225,7 @@ betweenexp_diff = lapply(1:3, function(df_2exp_n){
     df2 = data_30mln_noX[[m2]][, c(1, sort(c(data2$X1.1*2, data2$X1.1*2+1, data2$X2.1*2, data2$X2.1*2+1)))]
     df = merge(df1, df2, by="ensembl_gene_id")
     head(df)
-    PerformBinTestAIAnalysisForTwoConditions_knownCC(df, vect1CondReps=1:2, vect2CondReps=3:4, 
+    PerformBinTestAIAnalysisForTwoConditions_knownCC(df, vect1CondReps=1:2, vect2CondReps=3:4,
                                                      vect1CondRepsCombsCC=data2$CC, vect2CondRepsCombsCC=data2$CC.1,
                                                      Q=0.95, thr=10, thrUP=NA, thrType="each", minDifference=NA)
   })
@@ -235,7 +248,7 @@ interrep_exp_diff_df = do.call(rbind, lapply(1:3, function(i){
     data.frame(method = list_of_libprepnames[[i]],
                DiffASE_proportion = interrep_exp_diff_list[[i]]$BT_p,
                DiffASE_N = interrep_exp_diff_list[[i]]$BT_count,
-               test = "BT", 
+               test = "BT",
                what = "1 Experiment"),
     data.frame(method = list_of_libprepnames[[i]],
                DiffASE_proportion = interrep_exp_diff_list[[i]]$BTCC_p,
@@ -264,7 +277,7 @@ betweenexp_exp_diff_df = do.call(rbind, lapply(1:3, function(i){
     data.frame(method = paste(list_of_libprepnames[[mm[1]]], "vs", list_of_libprepnames[[mm[2]]]),
                DiffASE_proportion = betweenexp_diff_list[[i]]$BT_p,
                DiffASE_N = betweenexp_diff_list[[i]]$BT_count,
-               test = "BT", 
+               test = "BT",
                what = "2 Experiments"),
     data.frame(method = paste(list_of_libprepnames[[mm[1]]], "vs", list_of_libprepnames[[mm[2]]]),
                DiffASE_proportion = betweenexp_diff_list[[i]]$BTCC_p,
@@ -275,72 +288,73 @@ betweenexp_exp_diff_df = do.call(rbind, lapply(1:3, function(i){
 }))
 
 df_fig1A <- rbind(betweenexp_exp_diff_df, interrep_exp_diff_df)
-df_fig1A$method <- factor(df_fig1A$method, 
+df_fig1A$method <- factor(df_fig1A$method,
                              levels = c("NEBNext (100ng) vs SMARTseq (10ng)",
                                         "NEBNext (100ng) vs SMARTseq (0.1ng)",
                                         "SMARTseq (10ng) vs SMARTseq (0.1ng)",
                                         "NEBNext (100ng)",
                                         "SMARTseq (10ng)",
                                         "SMARTseq (0.1ng)"))
-df_fig1A$test <- factor(df_fig1A$test, labels = c("before correction", "QCC corrected")) 
+df_fig1A$test <- factor(df_fig1A$test, labels = c("before correction", "QCC corrected"))
 # Plotting
 
 
-figure_4A <- ggplot(df_fig1A, 
-       aes(method, DiffASE_N, col= method)) + 
+figure_4A <- ggplot(df_fig1A, aes(method, DiffASE_N, col= method)) +
   geom_boxplot() +
   theme_bw() +
   facet_grid(~ test) +
-  scale_color_manual(values=c("gray","gray","gray", "royalblue1","maroon2","olivedrab3")) +
-  ylab("Number of genes called differential") + 
+  scale_color_manual(values=c("black","black","black", "royalblue1","maroon2","olivedrab3")) +
+  ylab("Number of genes called differential") +
   xlab("") +
-  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), legend.position = "None", text = element_text(size=18))
 
-figure_4B <- ggplot(rbind(res62_df_all,res61_df_all), aes(x=method, y=FP_rate, col=experiment)) +
+
+df_fig4B <- rbind(res62_df_all,res61_df_all)
+df_fig4B$method <- factor(df_fig4B$method, labels = c("1 replicate,\nno correction", "2 replicates,\nno correction", "2 replicates,\nQCC correction"))
+
+figure_4B <- ggplot(df_fig4B, aes(x=experiment, y=FP_rate, col=experiment)) +
   geom_boxplot() +
+  facet_grid(~ method) +
   xlab("") +
   ylab("False positive rate") +
   theme_bw() +
-  theme(legend.position="bottom", text = element_text(size=18)) +
-  scale_x_discrete(labels=c("1 replicate,\nno correction", "2 replicates,\nno correction", "2 replicetes,\nQCC correction")) +
-  scale_color_manual(values=c("royalblue1","olivedrab3","maroon2"))
+  scale_color_manual(values=c("royalblue1","olivedrab3","maroon2")) +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), text = element_text(size=18), legend.position = "None")
+
+cowplot::save_plot(figure_4A, file="~/Dropbox (Partners HealthCare)/replicates_ASE/manuscript/Figures/fig.4A_legend.pdf", base_height = 10, base_width = 16)
+
 
 figure_4C <- grid.arrange(
-  ggplot() + 
+  ggplot() +
     geom_boxplot(data=CC_ov_sdp_df, aes(overdispersion_sqr, QCC, col=method)) +
     geom_point(data=CC_ov_sdp_df, aes(overdispersion_sqr, QCC, col=method)) +
     theme_bw() +
     scale_color_manual(values=c("royalblue1","olivedrab3","maroon2")) +
     xlab("Sqrt of overdispersion") +
     theme(legend.position= "None", text = element_text(size=18)) ,
-  ggplot() + 
+  ggplot() +
     geom_point(data=CC_ov_sdp_df, aes(sd_pair, QCC, col=method)) +
     theme_bw() +
-    scale_color_manual(values=c("royalblue1","olivedrab3","maroon2")) + 
+    scale_color_manual(values=c("royalblue1","olivedrab3","maroon2")) +
     xlab("Counts sd") +
     theme(legend.position= "None", text = element_text(size=18)) ,
-  ggplot() + 
-    geom_boxplot(data=CC_ov_sdp_df, aes(overdispersion_sqr, sd_pair, col=method)) +
-    geom_point(data=CC_ov_sdp_df, aes(overdispersion_sqr, sd_pair, col=method)) +
-    theme_bw() +
-    scale_color_manual(values=c("royalblue1","olivedrab3","maroon2")) + 
-    ylab("Counts sd") + xlab("Sqrt of overdispersion")+
-    theme(legend.position= "None", text = element_text(size=18)) ,
-  ncol=3
+  ncol=2
 )
 
 figure_4D <- ggplot() +
-  geom_point(data=dataCexlALLlogG1, aes(mc, vc, col = who), size=0.7, alpha=0.4) +
-  geom_line(data=data.frame(a=c(0,7.5), b=c(0,7.5)), aes(a,b), linetype="dashed") +
-  scale_y_continuous(breaks=c(0:6), labels=10**c(0:6), limits = c(0, 6)) +
-  scale_x_continuous(breaks=c(0:6), labels=10**c(0:6), limits = c(0, 6)) +
-  facet_grid(what ~ .) +
+  geom_point(data=dataCexlALLlogG1, aes(exp(mc), exp(vc), col = who), size=0.7, alpha=0.4) +
+  geom_abline(slope = 1, linetype="dashed") +
+  scale_y_log10() +
+  scale_x_log10() +
+  facet_grid(~what) +
   theme_bw() +
-  stat_smooth(data=dataCexlALLlogG1, aes(mc, vc, group=who), col="black", method = "lm", formula = (y ~ offset(x)))+
+  stat_smooth(data=dataCexlALLlogG1, aes(exp(mc), exp(vc), group=who), col="black", method = "lm", formula = (y ~ offset(x)))+
   scale_color_manual(values=c("royalblue1","olivedrab3","maroon2")) +
   ylab("Dispersion of gene coverages") + xlab("Mean gene coverage") +
-  theme(legend.position="None", text = element_text(size=18), axis.text.x = element_text(angle = 90)) +
+  theme(legend.position="None", text = element_text(size=18)) +
   coord_fixed()
+
+
 
 PLT_fig4 = plot_grid(
   plot_grid(figure_4A, figure_4B, labels = c("A", "B"), rel_widths = c(0.8, 0.8)),
@@ -349,4 +363,4 @@ PLT_fig4 = plot_grid(
   scale = c(0.9, 0.9, 0.9, 0.9, 0.9)
 )
 
-cowplot::save_plot(PLT_fig4, file="~/Dropbox (Partners HealthCare)/replicates_ASE/manuscript/Figures/fig.4_v1.pdf", base_height = 10, base_width = 16)
+cowplot::save_plot(PLT_fig4, file="~/Dropbox (Partners HealthCare)/replicates_ASE/manuscript/Figures/fig.4_v2.pdf", base_height = 10, base_width = 16)
